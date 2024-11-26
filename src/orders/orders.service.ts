fef47a65b7a2b8e0de65f8e4a6b105fe0f2f09cd
mijail pulgar <mpulgar@abatechnology.com>
2024-11-25 20:40:08 -0500

    feat: create orden dto update

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -15,7 +15,11 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
   }
 
   create(createOrderDto: CreateOrderDto) {
-    return this.order.create({ data: createOrderDto });
+    return {
+      service: 'Orders Microservice',
+      createOrderDto,
+    };
+    // return this.order.create({ data: createOrderDto });
   }
 
   async findAll(orderPaginationDto: OrderPaginationDto) {
