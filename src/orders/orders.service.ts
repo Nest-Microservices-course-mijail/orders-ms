54c66986500c31a70910f35bec78f3e6e5e6ec6a
mijail pulgar <mpulgar@abatechnology.com>
2024-11-26 09:31:22 -0500

    feat: get order by id with order item detail

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -84,12 +84,6 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
     } catch (error) {
       throw new RpcException(error);
     }
-
-    // return {
-    //   service: 'Orders Microservice',
-    //   createOrderDto,
-    // };
-    // return this.order.create({ data: createOrderDto });
   }
 
   async findAll(orderPaginationDto: OrderPaginationDto) {
@@ -117,7 +111,18 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
   }
 
   async findOne(id: string) {
-    const order = await this.order.findUnique({ where: { id } });
+    const order = await this.order.findFirst({
+      where: { id },
+      include: {
+        OrderItem: {
+          select: {
+            price: true,
+            quantity: true,
+            productId: true,
+          },
+        },
+      },
+    });
 
     if (!order) {
       throw new RpcException({
@@ -125,7 +130,20 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
         message: `Order with id ${id} not found`,
       });
     }
-    return order;
+
+    const productIds = order.OrderItem.map((orderItem) => orderItem.productId);
+    const products: any[] = await firstValueFrom(
+      this.productsClient.send({ cmd: 'validate_products' }, productIds),
+    );
+
+    return {
+      ...order,
+      OrderItem: order.OrderItem.map((orderItem) => ({
+        ...orderItem,
+        name: products.find((product) => product.id === orderItem.productId)
+          .name,
+      })),
+    };
   }
 
   async changeOrderStatus(changeOrderStatusDto: ChangeOrderStatusDto) {
